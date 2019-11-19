e1d9be716361bc71ed152de0e38e1ceb865c6abe
Cycalona Villani <cloniesmu@gmail.com>
2019-11-19 09:34:58 -0600

    created home page no style, added npm package for ejs

--- santa.js
+++ santa.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const santa = express()
 
+santa.set('views', 'views')
+santa.set('view engine', 'ejs')
+
 santa.get('/', function(req, res) {
-    res.send("Welcome to Santa's Workshop")
+    res.render('home-guest')
 })
 
 santa.listen(3000)
