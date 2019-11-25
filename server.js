c8d82a78445e3334dd10f421fb33f9b823885d9b
Nnamdi Iroham <niroham.yahoo.com>
2019-11-25 07:00:44 -0800

    major save

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
-const mongodb = require("mongodb");
 
-mongodb.connect(
-  process.env.MONGODB_URI || process.env.CONNECTIONSTRING,
+const router = require("./router");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.use("/", router);
+
+//Import Routes
+const postRoute = require("./routes/posts");
+
+app.use("/post", postRoute);
+
+// Connect To DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
-  function(err, client) {
-    module.exports = client.db();
-    const app = require("./app");
-    app.listen(process.env.PORT, test => console.log("listening!"));
-  }
+  () => console.log("Connected to DB")
 );
+
+app.listen(process.env.PORT, test => console.log("listening!"));
