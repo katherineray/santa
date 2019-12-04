633ece3b8d6a3465ff99d6f627b2c46b16ee44e4
Katherine Ray <katherine.ray@me.com>
2019-12-04 13:41:27 -0600

    heroku update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const itemRoutes = require("./posts");
-const userRoutes = require("./router");
+const userRoutes = require("./users");
 
 // routes
 router.use("/items", itemRoutes);
