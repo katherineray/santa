8e420a77178c0575f5a44d90e24f1db2c7fb61e4
Katherine Ray <katherine.ray@me.com>
2019-12-04 13:34:02 -0600

    heroku update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(router);
 
 // })
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -28,9 +28,9 @@ if (process.env.NODE_ENV === "production") {
   // Send every request to the React app
   // Define any API routes before this runs
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
   });
-}
+};
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
