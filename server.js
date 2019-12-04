b9e1e50f7a8ff22924594bcb79d006d5ab861e42
Katherine Ray <katherine.ray@me.com>
2019-12-04 12:56:25 -0600

    heroku update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = require("./routes");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const PORT = process.env.PORT || 5001;
 const dotenv = require("dotenv");
 dotenv.config();
