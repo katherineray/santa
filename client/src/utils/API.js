ee3c197ef4b9922eb4705015b8c19c75135d6958
Katherine Ray <katherine.ray@me.com>
2019-12-04 13:52:51 -0600

    heroku update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 export default {
   // Logins a user
-  login: function(userData) {
-    return axios.post("/users/login", userData);
+  login: function (userData) {
+    return axios.post("users/login", userData);
   },
   // Creates a user
-  signUp: function(userData) {
-    return axios.post("/users/", userData);
+  signUp: function (userData) {
+    return axios.post("users/", userData);
   },
-  getUser: function(userData) {
-    return axios.get("/users/" + userData);
+  getUser: function (userData) {
+    return axios.get("users/" + userData);
   },
-  updateUser: function(userId, userData) {
-    return axios.put("/users/" + userId, userData);
+  updateUser: function (userId, userData) {
+    return axios.put("users/" + userId, userData);
   }
 };
